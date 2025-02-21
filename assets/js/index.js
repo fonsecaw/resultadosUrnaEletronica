d3792f968ec738aa9d6c055d1769bf52fe959404
Maria <maduwincler.mw@hotmail.com>
2025-02-21 05:40:25 -0300

    feat: API e ano atual

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,24 +2,25 @@ function carregaDados() {
   fetch("https://mesariobarao.squareweb.app/apuracao")
     .then((response) => response.json())
     .then((dados) => {
-      document.querySelector("#total-votos").innerText = dados.total_votos;
+      document.querySelector("#total-votos").innerText = dados.totalGeralDeVotos;
 
       const listaCandidatos = document.querySelector(".lista-candidatos");
       listaCandidatos.innerHTML = ""; // Agora está FORA do loop (corrigido)
 
-      dados.candidatos.forEach(candidato => {
-        let percentual = (candidato.votos_recebidos / dados.total_votos) * 100;
+      dados.apuracao.forEach((apuracao, index) => {
+        const percentual = (apuracao.totalVotos / dados.totalGeralDeVotos) * 100;
+        console.log(index);
 
         const li = document.createElement("li");
         li.classList.add("card");
 
         const info = document.createElement("span");
         info.classList.add("left");
-        info.innerHTML = `<strong>${candidato.nome}</strong>`;
+        info.innerHTML = `<strong>${apuracao.nome}</strong>`;
 
         const votos = document.createElement("span");
         votos.classList.add("votos");
-        votos.innerHTML = `${candidato.votos_recebidos} votos`;
+        votos.innerHTML = `${apuracao.totalVotos} votos`;
 
         const textPerc = document.createElement("span");
         textPerc.classList.add("right");
@@ -35,7 +36,7 @@ function carregaDados() {
 
         const progresso = document.createElement("div");
         progresso.classList.add("progress-bar");
-        progresso.id = `progress-${candidato.id}`;
+        progresso.id = `progress-${index}`;
         progresso.style.width = "0%"; // Garante que a barra começa vazia
 
         barra.appendChild(progresso);
@@ -46,10 +47,16 @@ function carregaDados() {
         listaCandidatos.appendChild(li);
 
         // Atualiza a barra de progresso depois de adicionar ao DOM
-        atualizarProgresso(`progress-${candidato.id}`, percentual);
+        atualizarProgresso(`progress-${index}`, percentual);
       });
+      console.log(dados)
     })
     .catch(error => console.error("Erro ao carregar os dados:", error));
+
+    // Inserindo título com ano automático
+    const data = new Date();
+    const titulo = document.querySelector('.title');
+    titulo.innerText = `Resultados Oficiais da Eleição do Grêmio Estudantil ${data.getFullYear()}`
 }
 
 // Função para atualizar a barra de progresso dinamicamente
@@ -63,3 +70,4 @@ function atualizarProgresso(idBarra, valor) {
 
 // Chama a função ao carregar a página
 carregaDados();
+
